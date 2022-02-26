f2165cd0d6c6727962ab83258ff91cb8717b7310
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-02-26 20:33:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("btn").onclick = function(){
-    window.location.href = "https://discord.gg/Dkf5pk4UdY";
+document.getElementById("btn").onclick = function() {
+    window.location.href = "https://discord.gg/Dkf5pk4UdY"
 };
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+}
